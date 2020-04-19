1226ca5e6161b303137ea6bdc2fd68a0b3358dfe
tokyr <toky.rakoton@gmail.com>
2020-04-19 19:36:41 +0200

    Ajout des fichiers de paramètre permettant de déployer l'API dans le serveur

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!-k!i!2_gu9f*0s=3o@(w=4cbdc$i0rd95id1hzh$0j&iav@ii'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
